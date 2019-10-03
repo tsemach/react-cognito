4177f6888d8db65c7c2befecad44d869bae886ab
Tsemach Mizrachi <tsemach.mizrachi@gmail.com>
2019-10-03 21:50:49 +0300

    add gh-pages

---
+++ precache-manifest.4c85b49973e4d3250908952c0f8336c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a736828aa17e8ea0f8f",
+    "url": "/static/css/main.b6cd7ba8.chunk.css"
+  },
+  {
+    "revision": "4a736828aa17e8ea0f8f",
+    "url": "/static/js/main.2666ac03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "17c57b17f481181a412f",
+    "url": "/static/css/2.a6925145.chunk.css"
+  },
+  {
+    "revision": "17c57b17f481181a412f",
+    "url": "/static/js/2.3b48f440.chunk.js"
+  },
+  {
+    "revision": "37a5fa0d7f5d0b84b2b4a05a4781c30b",
+    "url": "/index.html"
+  }
+];
