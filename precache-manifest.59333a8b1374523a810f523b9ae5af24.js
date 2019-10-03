a27ac5866bb888fcd5f896e43985abf65e6d4594
Tsemach Mizrachi <tsemach.mizrachi@gmail.com>
2019-10-03 22:19:54 +0300

    fix relative path

---
+++ precache-manifest.59333a8b1374523a810f523b9ae5af24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2e33e8f48b0b0d1e329",
+    "url": "/react-cognito/static/css/main.b6cd7ba8.chunk.css"
+  },
+  {
+    "revision": "d2e33e8f48b0b0d1e329",
+    "url": "/react-cognito/static/js/main.c7334d42.chunk.js"
+  },
+  {
+    "revision": "45356b452819f1787c79",
+    "url": "/react-cognito/static/js/runtime~main.19eac96e.js"
+  },
+  {
+    "revision": "795cf0471af61c6591c0",
+    "url": "/react-cognito/static/css/2.a6925145.chunk.css"
+  },
+  {
+    "revision": "795cf0471af61c6591c0",
+    "url": "/react-cognito/static/js/2.b97358f7.chunk.js"
+  },
+  {
+    "revision": "b9a45a9f5b8df1e46b8cdfb0da67db1b",
+    "url": "/react-cognito/index.html"
+  }
+];
