7bef15fadd2b46fdfad958f5acb8b2f613139610
Tsemach Mizrachi <tsemach.mizrachi@gmail.com>
2019-10-03 21:10:17 +0300

    add github-pages

---
+++ build/precache-manifest.14574347bbce1c5e1b56eaeabd55228d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "666e2768fd3be3392a26",
+    "url": "/static/css/main.b6cd7ba8.chunk.css"
+  },
+  {
+    "revision": "666e2768fd3be3392a26",
+    "url": "/static/js/main.b3975f90.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b72177ca0ef16817c00b",
+    "url": "/static/css/2.a6925145.chunk.css"
+  },
+  {
+    "revision": "b72177ca0ef16817c00b",
+    "url": "/static/js/2.f542b6f4.chunk.js"
+  },
+  {
+    "revision": "656264582190a9984640282cd40512cf",
+    "url": "/index.html"
+  }
+];
